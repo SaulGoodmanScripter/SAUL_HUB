b68e9b1af094b6c703ff620707f466af22804e5b
SaulGoodmanScripter <azamat.tk.2000@gmail.com>
2025-12-01 11:44:40 +0300

    Create NewYearIcon.lua

---
+++ NewYearIcon.lua
@@ -0,0 +1,38 @@
+return {
+    Spritesheets = {
+        ["christmas"] = "rbxassetid://122751026646817",
+    },
+    
+    Icons = {
+        ["christmas-tree"] = {
+            ImageRectPosition = Vector2.new(0, 0),
+            ImageRectSize = Vector2.new(96, 96),
+            Image = "christmas",
+        },
+        ["gift"] = {
+            ImageRectPosition = Vector2.new(96, 0),
+            ImageRectSize = Vector2.new(96, 96),
+            Image = "christmas",
+        },
+        ["snowflake"] = {
+            ImageRectPosition = Vector2.new(192, 0),
+            ImageRectSize = Vector2.new(96, 96),
+            Image = "christmas",
+        },
+        ["snowman"] = {
+            ImageRectPosition = Vector2.new(0, 96),
+            ImageRectSize = Vector2.new(96, 96),
+            Image = "christmas",
+        },
+        ["bell"] = {
+            ImageRectPosition = Vector2.new(96, 96),
+            ImageRectSize = Vector2.new(96, 96),
+            Image = "christmas",
+        },
+        ["sleigh"] = {
+            ImageRectPosition = Vector2.new(192, 96),
+            ImageRectSize = Vector2.new(96, 96),
+            Image = "christmas",
+        },
+    }
+}
